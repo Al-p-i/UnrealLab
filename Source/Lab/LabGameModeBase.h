450dad70afc6dcfd216e2c1e3a4400418a41ed97
a.pomosov <a.pomosov@alibaba-inc.com>
2017-12-26 22:21:33 +0300

    Initial commit

---
+++ Source/Lab/LabGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LabGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAB_API ALabGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
